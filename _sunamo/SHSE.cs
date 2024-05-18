e71a4dd74084a5edf845e421812e950f0ca3a25f
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-18 13:24:56 +0200

    Buildable fix

--- _sunamo/SHSE.cs
+++ _sunamo/SHSE.cs
@@ -1,22 +1,22 @@
-namespace SunamoCl;
-internal class SHSE
-{
-    public static string ConvertTypedWhitespaceToString(string delimiter)
-    {
-        const string nl = @"
-";
+//namespace SunamoCl;
+//internal class SHSE
+//{
+//    public static string ConvertTypedWhitespaceToString(string delimiter)
+//    {
+//        const string nl = @"
+//";
 
-        switch (delimiter)
-        {
-            // must use \r\n, not Environment.NewLine (is not constant)
-            case "\\r\\n":
-            case "\\n":
-            case "\\r":
-                return nl;
-            case "\\t":
-                return "\t";
-        }
+//        switch (delimiter)
+//        {
+//            // must use \r\n, not Environment.NewLine (is not constant)
+//            case "\\r\\n":
+//            case "\\n":
+//            case "\\r":
+//                return nl;
+//            case "\\t":
+//                return "\t";
+//        }
 
-        return delimiter;
-    }
-}
+//        return delimiter;
+//    }
+//}
