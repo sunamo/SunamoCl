dd5a0c11f904bbf26c81aa6c32ae5ae534b14648
sunamo.cz <radek.jancik@sunamo.cz>
2024-09-15 12:18:37 +0200

    Added comment

--- SunamoCmdArgs/Data/CommonArgs.cs
+++ SunamoCmdArgs/Data/CommonArgs.cs
@@ -15,6 +15,8 @@ public class CommonArgs
 
     /// <summary>
     /// if I want to run the application in a folder other than the current one. In the default state, the only 2 attributes that do not need a switch are only the mode and args to the mode in each application
+    /// 
+    /// Krom toho ale musí zůstat zachováno i složka na druhém místě hned za módem! Využívá se to např. ve VS při rychlých akcích $(ProjectDir)
     /// </summary>
     [Option("RunInFolder", ResourceType = typeof(string))]
     public string RunInFolder { get; set; }
