e498a31ec94072aabfa931d74a1b098fad42eefd
sunamo.cz <radek.jancik@sunamo.cz>
2024-09-15 12:04:23 +0200

    24.9.15.2 Added RunInFolder prop

--- SunamoCmdArgs/Data/CommonArgs.cs
+++ SunamoCmdArgs/Data/CommonArgs.cs
@@ -12,4 +12,10 @@ public class CommonArgs
 
     [Option("Mode", ResourceType = typeof(string))]
     public string Mode { get; set; } = "";
+
+    /// <summary>
+    /// if I want to run the application in a folder other than the current one. In the default state, the only 2 attributes that do not need a switch are only the mode and args to the mode in each application
+    /// </summary>
+    [Option("RunInFolder", ResourceType = typeof(string))]
+    public string RunInFolder { get; set; }
 }
