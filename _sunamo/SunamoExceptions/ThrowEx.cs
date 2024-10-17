3a26edb0ba131f1a0fa4b5d86fbdc8ef438bcb58
sunamo.cz <radek.jancik@sunamo.cz>
2024-10-17 13:40:58 +0200

    Finished convert to refactored ThrowEx.cs

--- _sunamo/SunamoExceptions/ThrowEx.cs
+++ _sunamo/SunamoExceptions/ThrowEx.cs
@@ -1,31 +1,30 @@
-
 namespace SunamoCl._sunamo.SunamoExceptions;
-public partial class ThrowEx
+internal partial class ThrowEx
 {
 
-    public static bool Custom(Exception ex, bool reallyThrow = true)
+    internal static bool Custom(Exception ex, bool reallyThrow = true)
     { return Custom(Exceptions.TextOfExceptions(ex), reallyThrow); }
 
-    public static bool Custom(string message, bool reallyThrow = true, string secondMessage = "")
+    internal static bool Custom(string message, bool reallyThrow = true, string secondMessage = "")
     {
         string joined = string.Join(" ", message, secondMessage);
         string? str = Exceptions.Custom(FullNameOfExecutedCode(), joined);
         return ThrowIsNotNull(str, reallyThrow);
     }
 
-    public static bool CustomWithStackTrace(Exception ex) { return Custom(Exceptions.TextOfExceptions(ex)); }
-    public static bool DivideByZero() { return ThrowIsNotNull(Exceptions.DivideByZero(FullNameOfExecutedCode())); }
-    public static bool IsNull(string variableName, object? variable = null)
+    internal static bool CustomWithStackTrace(Exception ex) { return Custom(Exceptions.TextOfExceptions(ex)); }
+    internal static bool DivideByZero() { return ThrowIsNotNull(Exceptions.DivideByZero(FullNameOfExecutedCode())); }
+    internal static bool IsNull(string variableName, object? variable = null)
     { return ThrowIsNotNull(Exceptions.IsNull(FullNameOfExecutedCode(), variableName, variable)); }
 
-    public static bool IsNullOrEmpty(string argName, string argValue)
+    internal static bool IsNullOrEmpty(string argName, string argValue)
     { return ThrowIsNotNull(Exceptions.IsNullOrWhitespace(FullNameOfExecutedCode(), argName, argValue, true)); }
 
-    public static bool NotImplementedCase(object notImplementedName)
+    internal static bool NotImplementedCase(object notImplementedName)
     { return ThrowIsNotNull(Exceptions.NotImplementedCase, notImplementedName); }
 
     #region Other
-    public static string FullNameOfExecutedCode()
+    internal static string FullNameOfExecutedCode()
     {
         Tuple<string, string, string> placeOfExc = Exceptions.PlaceOfException();
         string f = FullNameOfExecutedCode(placeOfExc.Item1, placeOfExc.Item2, true);
@@ -66,7 +65,7 @@ public partial class ThrowEx
         return string.Concat(typeFullName, ".", methodName);
     }
 
-    public static bool ThrowIsNotNull(string? exception, bool reallyThrow = true)
+    internal static bool ThrowIsNotNull(string? exception, bool reallyThrow = true)
     {
         if (exception == null)
         {
@@ -81,7 +80,7 @@ public partial class ThrowEx
     }
 
     #region For avoid FullNameOfExecutedCode
-    public static bool ThrowIsNotNull(Exception exception, bool reallyThrow = true)
+    internal static bool ThrowIsNotNull(Exception exception, bool reallyThrow = true)
     {
         if (exception != null)
         {
@@ -91,27 +90,27 @@ public partial class ThrowEx
         return true;
     }
 
-    public static bool ThrowIsNotNull<A, B>(Func<string, A, B, string?> f, A ex, B message)
+    internal static bool ThrowIsNotNull<A, B>(Func<string, A, B, string?> f, A ex, B message)
     {
         string? exc = f(FullNameOfExecutedCode(), ex, message);
         return ThrowIsNotNull(exc);
     }
 
-    public static bool ThrowIsNotNull<A>(Func<string, A, string?> f, A ex)
+    internal static bool ThrowIsNotNull<A>(Func<string, A, string?> f, A ex)
     {
         string? exc = f(FullNameOfExecutedCode(), ex);
         return ThrowIsNotNull(exc);
     }
 
-    public static bool ThrowIsNotNull(Func<string, string?> f)
+    internal static bool ThrowIsNotNull(Func<string, string?> f)
     {
         string? exc = f(FullNameOfExecutedCode());
         return ThrowIsNotNull(exc);
     }
 
-    internal static void KeyAlreadyExists(Dictionary<string, Func<Task>> allActionsAsync, string key, string v)
+    internal static bool KeyAlreadyExists<T, U>(Dictionary<T, U> dictionary, T key, string dictionaryName)
     {
-        throw new NotImplementedException();
+        return ThrowIsNotNull(Exceptions.KeyAlreadyExists(FullNameOfExecutedCode(), dictionary, key, dictionaryName));
     }
     #endregion
     #endregion
