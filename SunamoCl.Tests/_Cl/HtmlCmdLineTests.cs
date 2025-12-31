1f32d5eb5cdd13ceb5366874f6af01c50af21d1d
Radek Jancik <radek.jancik@usys.cz>
2025-12-31 01:02:49 +0100

    25.12.31.1 feat: better naming for variables

--- SunamoCl.Tests/_Cl/HtmlCmdLineTests.cs
+++
@@ -1,7 +0,0 @@
-// variables names: ok
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-public class HtmlCmdLineTests
-{
-
-}
